8fe9c781fd41fb9319e0bcfaa4a6dda508d53b95
mhmdfrhn <frhn2707@gmail.com>
2023-06-23 17:59:07 +0700

    create blog design and layout

--- src/app/blog/[id]/page.jsx
+++ src/app/blog/[id]/page.jsx
@@ -1,5 +1,62 @@
-import React from "react";
+import Image from "next/image";
+import styles from "./page.module.css";
 
 export default function BlogPost() {
-  return <div>BlogPost Page</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.top}>
+        <div className={styles.info}>
+          <h1 className={styles.title}>
+            Lorem ipsum dolor sit amet consectetur adipisicing.
+          </h1>
+          <p className={styles.desc}>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laborum
+            itaque in fuga cupiditate atque facilis repellendus dignissimos
+            omnis consequatur, impedit, incidunt, veniam expedita voluptate
+            dicta.
+          </p>
+          <div className={styles.author}>
+            <Image
+              src="https://images.pexels.com/photos/3130810/pexels-photo-3130810.jpeg"
+              alt=""
+              width={40}
+              height={40}
+              className={styles.avatar}
+            />
+            <span className={styles.username}>John Doe</span>
+          </div>
+        </div>
+        <div className={styles.imageContainer}>
+          <Image
+            src="https://images.pexels.com/photos/3130810/pexels-photo-3130810.jpeg"
+            alt=""
+            fill={true}
+            className={styles.image}
+          />
+        </div>
+      </div>
+      <div className={styles.content}>
+        <p className={styles.text}>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione
+          molestias aperiam natus placeat nihil? Doloribus ullam atque,
+          accusantium quod iure totam odio cupiditate ea nesciunt fugit iusto
+          facilis deserunt blanditiis hic natus consequatur aliquid assumenda
+          nostrum sequi architecto quas praesentium vero eum. Ullam rem eligendi
+          cupiditate dignissimos atque delectus fuga consectetur blanditiis{" "}
+          <br /> necessitatibus optio modi exercitationem nobis harum est
+          facilis, non vel qui in! Obcaecati doloremque aliquid quia modi,
+          officiis, assumenda commodi animi cum voluptas dolorem quibusdam nam
+          dignissimos eveniet necessitatibus at eaque eum! Amet magni neque
+          repellendus vero perferendis necessitatibus possimus <br />
+          perspiciatis, tempora harum voluptatum eos eligendi unde eveniet
+          sapiente deserunt, voluptas illo placeat reprehenderit esse sint.
+          Asperiores error dolore pariatur nam rerum similique tempora ipsa
+          blanditiis voluptates labore alias voluptatem deleniti officiis natus
+          velit aspernatur dolor, beatae libero aliquam. Adipisci molestiae
+          aspernatur possimus aut ab minima, quod omnis similique quidem vero
+          facere ducimus voluptatibus saepe doloremque repellat? Dolorum.
+        </p>
+      </div>
+    </div>
+  );
 }
--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -1,5 +1,48 @@
+import Image from "next/image";
+import Link from "next/link";
 import styles from "./page.module.css";
 
 export default function Blog() {
-  return <div className={styles.container}>Blog Page</div>;
+  return (
+    <div className={styles.mainContainer}>
+      <Link href="/blog/testId" className={styles.container}>
+        <div className={styles.imgContainer}>
+          <Image
+            src="https://images.pexels.com/photos/3130810/pexels-photo-3130810.jpeg"
+            alt=""
+            className={styles.img}
+            width={400}
+            height={250}
+          />
+        </div>
+        <div className={styles.content}>
+          <h1 className={styles.title}>test</h1>
+          <p className={styles.desc}>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid
+            esse aut neque voluptatibus quidem alias tempore modi sunt fuga nam.
+            Quaerat aperiam error enim aliquam.
+          </p>
+        </div>
+      </Link>
+      <Link href="/blog/testId" className={styles.container}>
+        <div className={styles.imgContainer}>
+          <Image
+            src="https://images.pexels.com/photos/3130810/pexels-photo-3130810.jpeg"
+            alt=""
+            className={styles.img}
+            width={400}
+            height={250}
+          />
+        </div>
+        <div className={styles.content}>
+          <h1 className={styles.title}>test</h1>
+          <p className={styles.desc}>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid
+            esse aut neque voluptatibus quidem alias tempore modi sunt fuga nam.
+            Quaerat aperiam error enim aliquam.
+          </p>
+        </div>
+      </Link>
+    </div>
+  );
 }
