e5d865007b42b38cd2cf3110301c631581a687f5
mhmdfrhn <frhn2707@gmail.com>
2023-06-23 18:00:21 +0700

    final modified dummy page and component

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -1,5 +1,27 @@
+import Button from "@/components/button/Button";
+import Image from "next/image";
 import styles from "./page.module.css";
 
 export default function Contact() {
-  return <div className={styles.container}>Contact</div>;
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.title}>Let's Keep in Touch</h1>
+      <div className={styles.content}>
+        <div className={styles.imgContainer}>
+          <Image src="/contact.png" fill={true} alt="" className={styles.img} />
+        </div>
+        <div className={styles.form}>
+          <input type="text" placeholder="Name" className={styles.input} />
+          <input type="email" placeholder="Email" className={styles.input} />
+          <textarea
+            className={styles.textArea}
+            cols="30"
+            rows="10"
+            placeholder="messages"
+          ></textarea>
+          <Button url="#" text="Send" />
+        </div>
+      </div>
+    </div>
+  );
 }
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,6 +1,7 @@
 import styles from "./page.module.css";
 import Hero from "public/hero.png";
 import Image from "next/image";
+import Button from "@/components/button/Button";
 
 export default function Home() {
   return (
@@ -13,7 +14,7 @@ export default function Home() {
           Turning your Idea into Reality. We bring together the teams from the
           global tech industry
         </p>
-        <button className={styles.btn}>See Our Works</button>
+        <Button url="/portfolio" text="See Our Works" />
       </div>
       <div className={styles.item}>
         <Image src={Hero} alt="hero-img" className={styles.hero} />
